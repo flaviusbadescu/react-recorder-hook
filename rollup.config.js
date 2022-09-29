a39c9215464167acd1b7688da848bda60bba790e
flaviusbadescu <flaviusbadescu@gmail.com>
2022-09-29 10:16:49 +0300

    support ts

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,4 @@
-import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
-import external from "rollup-plugin-peer-deps-external";
 
 const packageJson = require("./package.json");
 
@@ -11,21 +8,8 @@ export default [
     output: [
       {
         file: packageJson.main,
-        format: "cjs",
-        sourcemap: true,
-        name: "react-ts-lib",
-      },
-      {
-        file: packageJson.module,
-        format: "esm",
-        sourcemap: true,
       },
     ],
-    plugins: [
-      external(),
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
-    ],
+    plugins: [typescript({ tsconfig: "./tsconfig.json" })],
   },
 ];
